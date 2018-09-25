f67e4abb8d1d7e127ad05b5e4dc41de0c4c415ee
Daniel Holmelund Andersen <daniel@holmelund.net>
2018-09-25 21:41:21 +0200

    Inital commit, created routing module, added hotel pages, added paths

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AccommodationsComponent } from './accommodations/accommodations.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { FoodAndBeveragesComponent } from './food-and-beverages/food-and-beverages.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { OffersComponent } from './offers/offers.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+
+const appRoutes: Routes = [
+    { path: 'accomodations', component: AccommodationsComponent },
+    { path: 'celebrations', component: CelebrationsComponent },
+    { path: 'food-and-beverages', component: FoodAndBeveragesComponent },
+    { path: 'lifestyle', component: LifestyleComponent },
+    { path: 'offers', component: OffersComponent },
+    { path: 'weddings', component: WeddingsComponent },
+    { path: 'sign-in', component: SignInComponent },
+    { path: 'sign-up', component: SignUpComponent }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(
+            appRoutes
+          )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccommodationsComponent } from './accommodations/accommodations.component';
+import { RoomsComponent } from './accommodations/rooms/rooms.component';
+import { FoodAndBeveragesComponent } from './food-and-beverages/food-and-beverages.component';
+import { RestaurantsComponent } from './food-and-beverages/restaurants/restaurants.component';
+import { BarsComponent } from './food-and-beverages/bars/bars.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { WellnessAndSpaComponent } from './lifestyle/wellness-and-spa/wellness-and-spa.component';
+import { ResortActivitiesComponent } from './lifestyle/resort-activities/resort-activities.component';
+import { ExperiencesComponent } from './lifestyle/experiences/experiences.component';
+import { ExcursionsComponent } from './lifestyle/excursions/excursions.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { WesternWeddingsComponent } from './weddings/western-weddings/western-weddings.component';
+import { ThaiWeddingsComponent } from './weddings/thai-weddings/thai-weddings.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { OffersComponent } from './offers/offers.component';
+import { AuthComponent } from './auth/auth.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccommodationsComponent,
+    RoomsComponent,
+    FoodAndBeveragesComponent,
+    RestaurantsComponent,
+    BarsComponent,
+    LifestyleComponent,
+    WellnessAndSpaComponent,
+    ResortActivitiesComponent,
+    ExperiencesComponent,
+    ExcursionsComponent,
+    WeddingsComponent,
+    WesternWeddingsComponent,
+    ThaiWeddingsComponent,
+    CelebrationsComponent,
+    OffersComponent,
+    AuthComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
